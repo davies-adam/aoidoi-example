0f787252a49f8f466f3329160d58da8fa0f71179
Adam Davies <adam.davies@outlook.com>
2014-04-03 20:52:51 +0000

    Init

---
+++ server.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+import flask
+import json
+
+app = flask.Flask(__name__)
+app.config.from_pyfile('config.py')
+
+@app.route('/works/<author>/<name>')
+def main(author, name):
+  path = "./works/" + author + "/" + name +".json"
+  with open(path) as file:
+    info = file.read()
+  return flask.render_template("main.html", data=json.loads(info))
